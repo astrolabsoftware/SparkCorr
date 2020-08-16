dd03045c68f13d320ce96e279883180bd6081cf0
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2020-08-16 19:36:30 +0200

    add args to script

--- runlocal.sh
+++ runlocal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION="0.1"
-
 myexec="ParamFile"
+args=$*
 
 # Package it
+VERSION="0.1"
 SCALA_VERSION_SPARK=2.11
 SCALA_VERSION=2.11.12
 
@@ -18,7 +18,7 @@ fi
 
 cmd="spark-submit $SPARKOPTS \
      --class com.sparkcorr.IO.$myexec \
-     $PWD/target/scala-${SCALA_VERSION_SPARK}/sparkcorr_${SCALA_VERSION_SPARK}-$VERSION.jar $PWD/test.txt"
+     $PWD/target/scala-${SCALA_VERSION_SPARK}/sparkcorr_${SCALA_VERSION_SPARK}-$VERSION.jar $args"
 
 
 echo $cmd
