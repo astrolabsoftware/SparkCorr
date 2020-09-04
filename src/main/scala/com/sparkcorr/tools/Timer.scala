b5e705be3667a87e6e90495b36af9d1d8bde7e06
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2020-09-04 22:47:56 +0200

    timer

---
+++ src/main/scala/com/sparkcorr/tools/Timer.scala
@@ -0,0 +1,21 @@
+
+package com.sparkcorr.tools
+
+//Timer
+class Timer (var t0:Double=System.nanoTime().toDouble,   var dt:Double=0)  extends Serializable  {
+
+  def time:Double=System.nanoTime().toDouble
+
+  def step:Double={
+    val t1 = System.nanoTime().toDouble
+    dt=(t1-t0)/1e9
+    t0=t1
+    dt
+  }
+  def print(msg:String):Unit={
+    step
+    val sep="----------------------------------------"
+    println(f"\n$msg: $dt%.1fs (${dt/60}%.2fmin)\n"+sep)
+
+  }
+}
