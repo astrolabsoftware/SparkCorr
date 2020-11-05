3ce5e31ed210da04d3a673fa15bf7c11798af030
Stephane Plaszczynski <plaszczy@lal.in2p3.fr>
2020-11-05 14:12:11 +0100

    update binng csv

--- scripts/bin.py
+++ scripts/bin.py
@@ -6,10 +6,17 @@ p=pandas.read_csv("sa_binning.csv",sep="\s+")
 
 #cols=[pp for pp in p.columns if pp!="Nj" and pp !="NpixJ(k)"]  
 
-cols=['w','Nd', 'NpixD(M)','tu','Nj', 'NpixJ(k)']
+cols=['td','w','Nd', 'NpixD(M)','tu','Nj', 'NpixJ(k)']
 
 
 
 
 
 p[cols].to_latex("hp_setup.tex",index=True) 
+
+
+p1=psa[['td','tu','w','Nd','NpixD(M)']] 
+p2=pcs[['Nj', 'NpixJ(k)']]
+pandas.concat([p1,p2],axis=1) 
+
+p.to_latex("mixed_setup.tex",index=True) 
